110156f87b560d96f3d1fdbf5637b42df9a5383d
Isabela Siqueira <15924970+Isabela192@users.noreply.github.com>
2023-07-12 15:05:23 -0300

    Adding models

--- petcompanyblog/__init__.py
+++ petcompanyblog/__init__.py
@@ -1,8 +1,26 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
+### DATABASE SETUP ####
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+### Login Setup ###
+login_manger = LoginManager()
+login_manger.init_app(app)
+login_manger.login_view = 'users.login'
+
+#### App Setup ###
 from petcompanyblog.core.views import core
 from petcompanyblog.error_pages.handlers import error_pages
 

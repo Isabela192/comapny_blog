88691045d00ac21f9548d336aac3612810c0d0be
Isabela <isabela7011@gmail.com>
2023-07-16 22:41:47 -0300

    Blog post funcionallity

--- petcompanyblog/__init__.py
+++ petcompanyblog/__init__.py
@@ -24,7 +24,9 @@ login_manger.login_view = 'users.login'
 from petcompanyblog.core.views import core
 from petcompanyblog.error_pages.handlers import error_pages
 from petcompanyblog.users.views import users
+from petcompanyblog.blog_posts.views import blog_posts
 
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)

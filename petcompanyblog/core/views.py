d55f5477880fb98aeaa62ad0e12c2d6892dc3397
Isabela <isabela7011@gmail.com>
2023-07-16 23:20:49 -0300

    testing 2

--- petcompanyblog/core/views.py
+++ petcompanyblog/core/views.py
@@ -7,9 +7,9 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
-    blog_post = BlogPost.query.order_by(BlogPost.data.desc()).paginate(page=page, per_page=5)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
 
-    return render_template('index.html', blog_post=blog_post)
+    return render_template('index.html', blog_posts=blog_posts)
 
 
 @core.route('/info')
